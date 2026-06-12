fe6e492ca82e65b938317af918728a5cc35284b0
dinod <dinodtharinda2001@gmail.com>
2026-06-12 16:42:58 +0530

    Initial Commit

---
+++ datasourse-to-diffable-datasource/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  datasourse-to-diffable-datasource
+//
+//  Created by Dinod Tharinda on 2026-06-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
